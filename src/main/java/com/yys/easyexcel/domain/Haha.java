e237379eb2b690eccee7eae87910ee9085ef7a2a
yangyansheng0527 <76691407+yangyansheng0527@users.noreply.github.com>
2021-03-27 14:26:23 +0800

    提交git测试文件

---
+++ src/main/java/com/yys/easyexcel/domain/Haha.java
@@ -0,0 +1,12 @@
+package com.yys.easyexcel.domain;
+
+/**
+ * @version 1.0
+ * @Copyright (c) 宝库技术团队 www.baoku.com
+ * @Package com.yys.easyexcel.domain
+ * @Project easyexcel
+ * @authur yangyansheng yangyansheng@baoku.com
+ * @date 2021/3/27 14:21
+ */
+public class Haha {
+}
